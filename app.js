d02f248fcbb887229eaaf4656bd02da86ccf21f9
59160922 <59160922@go.buu.ac.th>
2019-08-17 10:16:35 +0700

    pokemon new format

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const express = require('express')
 const app = express() //create express app invoke express
 const port =3000
 app.use(express.json())
-var pokemons = [
-                {name:'Cocoon' ,type:'Bug'},
-                {name:'Nidorina',type:'poison'},
-                { name:'Arbok',type:'Bug'}
-               ]
 
+class pokemon{
+  constructor(name,type){
+    this.name = name
+    this.type = type
+    this.id = null
+  }
+}
 
+var pokemons = []
+pokemons.push(createPokemon('Cocoon','Bug'))
+
+function generateNewId(num){
+  num+=1
+  return num
+}
+function createPokemon(name,type){
+  let p = new pokemon(name,type)
+  p.id = generateNewId(pokemons.length)
+  return p    
+
+}
 
 app.get('/',(req,res) => res.send('Hello World!'))
 //app.post('/pokemon',(req,res) => res.send(pokemon))
@@ -16,7 +31,8 @@ app.get('/pokemon',(req,res) => res.send(pokemons))
 
 //POST /pokemon -> add pokemon to list
 app.post('/pokemon',(req,res)=>{
-  pokemons.push(req.body.body)
+  let p = createPokemon(req.body.name,req.body.type)
+  pokemons.push(p)
   res.sendStatus(201)
   console.log()
 })
